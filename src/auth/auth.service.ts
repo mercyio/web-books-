277a131e3a53f3823b2e9490b981fbb8e8811578
mercyio <mercydanke@gmail.com>
2024-02-19 23:39:06 +0100

    hot fix

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/schema/user.schema';
 
 
 @Injectable()
 export class AuthService {
   constructor (
     @InjectModel (User.name) private userModel:Model<User>,
-    @InjectModel (User.name) private profileModel:Model<Profile>){}
+    // @InjectModel (User.name) private profileModel:Model<Profile>
+    ){}
  
   create() {
     return 'This action adds a new auth';
